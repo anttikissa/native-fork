8257ca66aaddd06222b467cb0515c7ee8328bee7
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2019-03-04 11:28:53 +0200

    fork.cc: fix whitespace, remove unused variable

--- fork.cc
+++ fork.cc
@@ -1,18 +1,16 @@
 #include <node.h>
 #include <unistd.h>
 
-int numberOfCalls = 0;
-
-void Fork(const v8::FunctionCallbackInfo<v8::Value>& args)
+void Fork(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
-    auto pid = fork();
+	auto pid = fork();
 
-    args.GetReturnValue().Set(pid);
+	args.GetReturnValue().Set(pid);
 }
 
 void Initialize(v8::Local<v8::Object> exports)
 {
-    NODE_SET_METHOD(exports, "fork", Fork);
+	NODE_SET_METHOD(exports, "fork", Fork);
 }
 
 NODE_MODULE(module_name, Initialize)
